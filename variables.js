865e05805f89b82ee0c94857c7987696f04ddff8
Alexander Uspensky <uspensky@live.de>
2023-03-04 14:41:38 +0300

    создание переменных в JavaScript

---
+++ variables.js
@@ -0,0 +1,6 @@
+// Anything following double slashes is an English-language comment.
+// Read the comments carefully: the explain the JavaScript code.
+
+// A variable is a symbolic name for a value.
+// Variable are declared with let keyword:
+let x;                    // Declare a variable named x.
