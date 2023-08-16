839d339f304806f027cdc3d83183d12ddaaeef5b
Alexander Uspensky <auspensky@live.ru>
2023-08-16 09:54:34 +0300

    arithmetic operators

---
+++ expression.js
@@ -0,0 +1,8 @@
+// One of the most common wayus to form expressions in JavaScript is to use operators:
+// Operators act on values (the operands) to produce a new value.
+// Arithmetic operators are some of the simplest:
+3 + 2	// => 5: addition
+3 - 2	// => 1: subtraction
+3 * 2	// => 6: multiplication
+3 / 2	// => 1.5 division
+points[1].x - points[0].x	// => 1: more complicated operands also work
