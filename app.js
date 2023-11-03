2c7a6961f8c3d30f2765b95d78b8aa9cdb271a8d
Alexander Uspensky <uspensky@live.de>
2023-11-04 01:32:48 +0300

    тип данных string

---
+++ app.js
@@ -0,0 +1,19 @@
+{
+  let x = '[eks]';
+  console.log(x);
+}
+
+var y = '[waɪ]';
+{
+  console.log(y); // блок кода
+}
+
+{
+  const pi = 3.14;
+  // const pi = 4; нельзя так делать
+  console.log(pi);
+}
+
+
+
+
---
+++ string.js
@@ -0,0 +1,3 @@
+let language = "JavaScript";
+let message = `Let's learn ${language}`;
+console.log(message);
